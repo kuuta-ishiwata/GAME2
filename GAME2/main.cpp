f6589e117e93da7a19013024d5a0918ee817e880
k022g0029@g.neec.ac.jp <k022g0029@g.neec.ac.jp>
2023-09-08 10:07:37 +0900

    AL

--- GAME2/main.cpp
+++ GAME2/main.cpp
@@ -1,4 +1,5 @@
 #include <Novice.h>
+#define _USE_MATH_DEFINES
 #include <math.h>
 
 const char kWindowTitle[] = "学籍番号";
@@ -16,9 +17,9 @@ int date[MAPY][MAPX]
 {
 
 	1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-	1,0,2,0,3,0,4,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-	1,0,2,0,3,0,4,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-	1,0,2,0,3,0,4,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+	1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+	1,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+	1,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
 	1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
 	1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
 	1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
@@ -51,6 +52,7 @@ struct Player
 	int timer;
 };
 
+
 struct book
 {
 	Vector2 position;
@@ -61,8 +63,16 @@ struct book
 
 enum BLOCK
 {
+
 	NOME,
 	block
+   
+};
+
+enum  Book
+{
+	None,
+	book1
 };
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -75,13 +85,26 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
-	int posX = 50;
-	int posY = 50;
-
+	
 	int a = 0;
 	int b = 0;
 	int flag = false;
 	int flag2 = false;
+
+	int countflag = false;
+	int countflag2 = false;
+	int countflag3 = false;
+	int countflag4 = false;
+	int countflag5 = false;
+	int countflag6 = false;
+	int countflag7 = false;
+	int countflag8 = false;
+	int countflag9 = false;
+	int countflag10 = false;
+	int countflag11 = false;
+	int countflag12 = false;
+	int countflag13 = false;
+
 
 	Player player;
 	player.position = { 95, 45 };
@@ -108,10 +131,23 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Book5.position = { 350,50 };
 	Book5.radius = { 10,40 };
 	Book5.flag = false;
+	
+	int timer = 0;
+	int count = 0;
 
 	int grahHandle = Novice::LoadTexture("./GJ3_Frame.png");
-	
-	
+	int count1 = Novice::LoadTexture("./count.png");
+	int count2 = Novice::LoadTexture("./count2.png");
+	int count3 = Novice::LoadTexture("./count3.png");
+	int count4 = Novice::LoadTexture("./count4.png");
+	int count5 = Novice::LoadTexture("./count5.png");
+	int count6 = Novice::LoadTexture("./count6.png");
+	int count7 = Novice::LoadTexture("./count7.png");
+	int count8 = Novice::LoadTexture("./count8.png");
+	int count9 = Novice::LoadTexture("./count9.png");
+	int count10 = Novice::LoadTexture("./count10.png");
+	int count11 = Novice::LoadTexture("./count30.png");
+	int count12 = Novice::LoadTexture("./count60.png");
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -161,12 +197,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 				if (player.position.x == Book.position.x - 5 && player.position.y == Book.position.y - 5)
 				{
-					flag = true;
+
+						flag = true;
+
 				}
 
 			}
 			if (flag == true)
 			{
+			
 
 				player.position.x = Book.position.x - 5;
 				player.position.y = Book.position.y - 5;
@@ -188,11 +227,15 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 					Book.position.y += player.speed.y;
 				}
 
+
 			}
+
 
 			if (keys[DIK_A] == preKeys[DIK_A] == 0)
 			{
+
 				flag = false;
+
 			}
 
 			//Book2
@@ -201,7 +244,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		{
 			if (player.position.x == Book2.position.x - 5 && player.position.y == Book2.position.y - 5)
 			{
+
 				Book2.flag = true;
+
 			}
 		}
 
@@ -212,6 +257,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 			if (keys[DIK_LEFT] && preKeys[DIK_LEFT] == 0)
 			{
+
 				Book2.position.x -= player.speed.x;
 
 			}
@@ -225,6 +271,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			{
 
 				Book2.position.y -= player.speed.y;
+
 			}
 
 			if (keys[DIK_DOWN] && preKeys[DIK_DOWN] == 0)
@@ -238,25 +285,31 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		if (keys[DIK_A] == preKeys[DIK_A] == 0)
 		{
+
 			Book2.flag = false;
 
 		}
 		
+
 		//Book3
 		if (keys[DIK_SPACE] == preKeys[DIK_SPACE] == 0)
 		{
 
-			if (player.position.x == Book3.position.x - 5 && player.position.y == Book3.position.y - 5)
+			if (player.position.x == Book3.position.x - 5&& player.position.y == Book3.position.y - 5)
 			{
+
 				Book3.flag = true;
+
 			}
 
 		}
+
 		if (Book3.flag == true)
 		{
 
 			player.position.x = Book3.position.x - 5;
 			player.position.y = Book3.position.y - 5;
+
 
 			if (keys[DIK_LEFT] && preKeys[DIK_LEFT] == 0)
 			{
@@ -275,43 +328,63 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 				Book3.position.y += player.speed.y;
 			}
 
+		
 		}
 
 		if (keys[DIK_A] == preKeys[DIK_A] == 0)
 		{
+
 			Book3.flag = false;
+
 		}
 
 
 		if (keys[DIK_S] == preKeys[DIK_S] == 0)
 		{
+
+
 			if (Book.position.x == Book3.position.x && Book.position.x == Book3.position.y)
 			{
+
+				flag = true;
+
 				if (keys[DIK_LEFT] && preKeys[DIK_LEFT] == 0)
 				{
+
 					Book.position.x -= player.speed.x;
+
 				}
 				if (keys[DIK_RIGHT] && preKeys[DIK_RIGHT] == 0)
 				{
+
 					Book.position.x += player.speed.x;
+
 				}
 				if (keys[DIK_UP] && preKeys[DIK_UP] == 0)
 				{
+
 					Book.position.y -= player.speed.y;
+
 				}
 				if (keys[DIK_DOWN] && preKeys[DIK_DOWN] == 0)
 				{
+
 					Book.position.y += player.speed.y;
+
 				}
+
 			}
 
 			if (Book2.position.x == Book3.position.x && Book2.position.x == Book3.position.y)
 			{
+
+				Book2.flag = true;
+
 				if (keys[DIK_LEFT] && preKeys[DIK_LEFT] == 0)
 				{
 					Book2.position.x -= player.speed.x;
-
 				}
+
 				if (keys[DIK_RIGHT] && preKeys[DIK_RIGHT] == 0)
 				{
 
@@ -330,16 +403,34 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 					Book2.position.y += player.speed.y;
 
 				}
+
 			}
+
 		}
+
 
 		//Book4
 
 
 		//Book5
 
+	
+		timer++;
+		if (timer >= 60)
+		{
+			count = 1 ;
+		}
+		if (timer >= 120)
+		{
+			count = 2;
+		}
+		if (timer >= 180)
+		{
+			count = 3;
+		}
 
-
+		Novice::ScreenPrintf(200, 100, "%d", timer);
+		Novice::ScreenPrintf(200, 120, "%d", count);
 
 
 
@@ -364,18 +455,120 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			{
 				if (date[j][i] == block)
 				{
-
 					Novice::DrawBox(i * 32, j * 32, 35, 35, 0.0f, WHITE, kFillModeSolid);
-					
-					
+
 				}
+			}
+		}
+
+		for (int i = 0; i < MAPX; i++)
+		{
+			for (int j = 0; j < MAPY; j++)
+			{
+
+				if (date[j][i] == 2)
+				{
+
+					Novice::DrawBox(i * 32, j * 32, Book.radius.x, Book.radius.y, 0.0f, RED, kFillModeSolid);
+
+				}
+
 			}
 		}
 		Novice::DrawSprite(player.position.x, player.position.y, grahHandle, 1, 1, 0.0f, WHITE);
 		Novice::DrawBox(Book.position.x, Book.position.y, Book.radius.x, Book.radius.y, 0.0f, WHITE, kFillModeSolid);
 		Novice::DrawBox(Book2.position.x, Book2.position.y, Book2.radius.x, Book2.radius.y, 0.0f, BLACK, kFillModeSolid);
 		Novice::DrawBox(Book3.position.x, Book3.position.y, Book3.radius.x, Book3.radius.y, 0.0f, RED, kFillModeSolid);
+	
+		
+		timer++;
 
+		
+		if (countflag == false)
+		{
+			Novice::DrawSprite(400, 0, count12, 1, 1, 0.0f, WHITE);
+		}
+
+
+		if (timer >= 3000&& countflag2 == false )
+		{
+			countflag = true;
+			Novice::DrawSprite(400, 0, count11, 1, 1, 0.0f, WHITE);
+			
+		}
+		
+		if (timer >= 5000 && countflag3 == false)
+		{
+			countflag2 = true;
+			Novice::DrawSprite(400, 0, count10, 1, 1, 0.0f, WHITE);
+
+		}
+		if (timer >= 5100 && countflag4 == false)
+		{
+			countflag3 = true;
+			Novice::DrawSprite(400, 0, count9, 1, 1, 0.0f, WHITE);
+
+		}
+		if (timer >= 5200 && countflag5 == false)
+		{
+			countflag4 = true;
+			Novice::DrawSprite(400, 0, count9, 1, 1, 0.0f, WHITE);
+
+		}
+		if (timer >= 5300 && countflag6 == false)
+		{
+			countflag5 = true;
+			Novice::DrawSprite(400, 0, count8, 1, 1, 0.0f, WHITE);
+
+		}
+		if (timer >= 5400 && countflag7 == false)
+		{
+			countflag6 = true;
+			Novice::DrawSprite(400, 0, count7, 1, 1, 0.0f, WHITE);
+
+		}
+		if (timer >= 5400 && countflag8 == false)
+		{
+			countflag7 = true;
+			Novice::DrawSprite(400, 0, count6, 1, 1, 0.0f, WHITE);
+
+		}
+		if (timer >= 5500 && countflag9 == false)
+		{
+			countflag8 = true;
+			Novice::DrawSprite(400, 0, count5, 1, 1, 0.0f, WHITE);
+
+		}
+		if (timer >= 5600 && countflag10 == false)
+		{
+			countflag9 = true;
+			Novice::DrawSprite(400, 0, count4, 1, 1, 0.0f, WHITE);
+
+		}
+		if (timer >= 5700 && countflag11 == false)
+		{
+
+			countflag10 = true;
+			Novice::DrawSprite(400, 0, count3, 1, 1, 0.0f, WHITE);
+
+		}
+		if (timer >= 5800 && countflag12 == false)
+		{
+			countflag11 = true;
+			Novice::DrawSprite(400, 0, count2, 1, 1, 0.0f, WHITE);
+
+		}
+		if (timer >= 5900 && countflag13 == false)
+		{
+			countflag12 = true;
+			Novice::DrawSprite(400, 0, count1, 1, 1, 0.0f, WHITE);
+
+		}
+
+		if (timer >= 180)
+		{
+			count = 3;
+		}
 
 		///
 		/// ↑描画処理ここまで
